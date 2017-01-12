58082775974f8c54c39f2fb27bc820e9f16797f4
sdey0081 <sdey0081@gmail.com>
2017-01-12 22:18:20 +0800

    init proj

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Page1,
+    Page2
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Page1,
+    Page2
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
---
+++ src/pages/page1/page1.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-page1',
+  templateUrl: 'page1.html'
+})
+export class Page1 {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
